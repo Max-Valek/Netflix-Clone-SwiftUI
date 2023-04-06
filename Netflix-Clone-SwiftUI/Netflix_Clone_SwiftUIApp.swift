12e1a5f1658a99b1edfcc1ae6967eaab927108d5
Max Valek <valek.maxwell@gmail.com>
2023-04-06 13:29:02 -0500

    Initial Commit

---
+++ Netflix-Clone-SwiftUI/Netflix_Clone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_Clone_SwiftUIApp.swift
+//  Netflix-Clone-SwiftUI
+//
+//  Created by Max Valek on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_Clone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
