26c7b8720191b363fd6a2786626a941a4462ad7a
Max Valek <valek.maxwell@gmail.com>
2023-04-07 08:51:06 -0500

    add featured movie to home view

--- Netflix-Clone-SwiftUI/Scenes/Home/HomeView.swift
+++ Netflix-Clone-SwiftUI/Scenes/Home/HomeView.swift
@@ -12,6 +12,8 @@ struct HomeView: View {
         
         ScrollView {
             
+            featured
+            
             VStack(alignment: .leading) {
                 Text("Trending")
                     .fontWeight(.bold)
@@ -47,5 +49,68 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
             .preferredColorScheme(.dark)
+    }
+}
+
+extension HomeView {
+    
+    @ViewBuilder
+    var featured: some View {
+        ZStack {
+            Image("better_call_saul")
+                .resizable()
+                .scaledToFill()
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            VStack {
+                
+                Spacer()
+                
+                HStack(spacing: 2) {
+                    Text("Offbeat")
+                    Text("•")
+                        .foregroundColor(Color(.systemOrange))
+                    Text("Violent")
+                    Text("•")
+                        .foregroundColor(Color(.systemOrange))
+                    Text("Horror Comedy")
+                    Text("•")
+                        .foregroundColor(Color(.systemOrange))
+                    Text("Ensemble")
+                }
+                .font(.caption).bold()
+                
+                HStack {
+                    
+                    Button {
+                        // play button
+                    } label: {
+                        Image(systemName: "play.fill")
+                        Text("Play")
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 4)
+                    .foregroundColor(.black)
+                    .background(.white, in: RoundedRectangle(cornerRadius: 4))
+                    
+                    Button {
+                        // add to list
+                    } label: {
+                        Image(systemName: "plus")
+                        Text("My List")
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 4)
+                    .foregroundColor(.white)
+                    .background(Color(.darkGray), in: RoundedRectangle(cornerRadius: 4))
+
+                }
+                
+            }
+            .padding(.bottom)
+        }
+        .frame(width: 330)
+        .padding(.bottom, 40)
+        .shadow(color: Color.secondary, radius: 10)
     }
 }
