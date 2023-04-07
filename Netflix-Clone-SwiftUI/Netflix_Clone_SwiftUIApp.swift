b5342e51856b4d87385234908da0738ad0b07d46
Max Valek <valek.maxwell@gmail.com>
2023-04-07 10:39:12 -0500

    debug

--- Netflix-Clone-SwiftUI/Netflix_Clone_SwiftUIApp.swift
+++ Netflix-Clone-SwiftUI/Netflix_Clone_SwiftUIApp.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct Netflix_Clone_SwiftUIApp: App {
+    // I was trying to get rid of "[SceneConfiguration] Info.plist contained no UIScene configuration dictionary (looking for configuration named "(no name)")" error
+//    init() {
+//        let configuration = UISceneConfiguration(name: "MainScreenSceneConfiguration",
+//                                                 sessionRole: .windowApplication)
+//        UIApplication.shared.requestSceneSessionActivation(nil, userActivity: nil, options: nil, errorHandler: nil)
+//    }
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
